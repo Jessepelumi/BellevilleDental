1269ff0e6684095f41e8875370e0283ef59d95ea
Jesse <jesseoluwapelumi01@gmail.com>
2023-05-09 11:53:38 +0100

    21st commit

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,108 +1,110 @@
 AOS.init();
 
+// Prodcut content 
 const products = [
     {
-        name: "Oral-B Rechargeable Toothbrush",
-        value: "2.3B",
+        name: "Rechargeable Toothbrush",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/71eU7ZuVXYL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Water Dental Flosser",
-        value: "198M",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/710XJeult+L._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
-        name: "Rechargeable Water Dental Flosser",
-        value: "100M",
+        name: "Water Dental Flosser",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/71eU7ZuVXYL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Whitening Gel Syringes",
-        value: "149M",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/716ROIefM5L._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
-        name: "GUM - Advanced Dental Picks",
-        value: "2.5B",
+        name: "Advanced Dental Picks",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/71hcySeE6kL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
-        name: "Oral-B Glide Pro-Health Dental Floss",
-        value: "187M",
+        name: "Glide Pro-Health Dental Floss",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/61xcsiSxu4L._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
-        name: "Fixodent Denture Adhesive Cream)",
-        value: "392M",
+        name: "Fixodent Denture Adhesive Cream",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/61hr9OnQRUL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
-        name: "Glide Comfort Plus Dental Floss",
-        value: "123M",
+        name: "Glide Dental Floss",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/713evG6ywXL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Bitvae Water Dental flosser",
-        value: "209M",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/718WKXCnlyL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Aiocoid Dental Whitening",
-        value: "355M",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/71O2n683ToL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Bubble Gum Flavored Tooth Polish",
-        value: "3.1B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/816RxdrFkDL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Ultrasonic Cleaner for Dentures",
-        value: "2.9B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/61tuPluE2oL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Best Dental Care Finger Wipes",
-        value: "2.9B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/71ZlhGsDyqL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Dental Mixing Tips Impression",
-        value: "2.9B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/61ne50ydvyL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
-        name: "Dental Floss Portable Dispenser",
-        value: "2.9B",
+        name: "Dental Floss Dispenser",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/71xiveqbrQL._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Oral Care Cleaning Kit",
-        value: "2.9B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/71lZaOa6Z8L._AC_UL600_FMwebp_QL65_.jpg",
     },
     {
         name: "Electric Tooth Cleaning Tool",
-        value: "2.9B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/61hafunfb-L._AC_UL600_QL65_.jpg",
     },
     {
         name: "Coconut-Oil Infused Dental Floss",
-        value: "2.9B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/81L37TmKO0L._AC_UL600_QL65_.jpg",
     },
     {
         name: "Plackers Twin-Line Dental Flossers",
-        value: "2.9B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/714LjzCB8nL._AC_UL600_QL65_.jpg",
     },
     {
         name: "Expandable Dental Floss",
-        value: "2.9B",
+        value: "$99.99",
         photo: "https://m.media-amazon.com/images/I/61-dfekqjkL._AC_UL600_QL65_.jpg",
     }
 ];
 
+// product box
 const container = document.querySelector(".container");
 
 for (let product of products) {
@@ -112,27 +114,32 @@ for (let product of products) {
     const productEl = document.createElement("div");
     productEl.classList.add("box");
 
+    // product images
     const photoEl = document.createElement("img");
     photoEl.src = product.photo;
     photoEl.alt = product.name;
     photoEl.classList.add("img");
 
+    // product name
     const nameEl = document.createElement("p");
     nameEl.innerText = product.name;
-    nameEl.classList.add("name");
+    nameEl.classList.add("name", "fw-bold");
 
+    // product price
     const valueEl = document.createElement("p");
     valueEl.innerHTML = product.value + ` <span>${product.value}</span>`;
 
+    // product rating
     const rating_container = document.createElement("div");
     rating_container.classList.add("rating_container");
 
     const rating = document.querySelector(".rating");
 
+    // add to cart
     const purchase = document.createElement("button");
     purchase.innerText = "ADD TO CART";
 
-
+    // combine all elements into a single container
     rating_container.append(rating, purchase);
     productEl.append(nameEl, valueEl, purchase, rating_container);
     productBox.append(photoEl, productEl)
@@ -150,7 +157,7 @@ window.addEventListener('scroll', () => {
     }
 })
 
-
+// sliding ticker
 function updateTicker() {
     var ticker = document.getElementById("ticker");
     var currentDate = new Date().toLocaleDateString();
@@ -218,3 +225,22 @@ let visitorCount = localStorage.getItem('visitorCount') || 0
     visitorCount++
     localStorage.getItem('visitorCount', visitorCount)
     document.getElementById('visitorCount').innerHTML = visitorCount;
+
+// Rating
+const stars = document.querySelectorAll(".rating svg");
+stars.forEach((star, index1) => {
+    star.addEventListener("click", () => {
+        for (let i = 0; i < star.parentElement.children.length; i++) {
+            const st = star.parentElement.children.item(i);
+            if (st.classList.contains("bi-star-fill")) {
+                st.classList.remove("active")
+            }
+
+            let current = star
+            while (current != null) {
+                current.classList.add("active")
+                current = current.previousElementSibling
+            }
+        }
+    })
+})

26380427137a54aeccc2da60f71a1822f8d90526
Jesse <jesseoluwapelumi01@gmail.com>
2023-07-07 04:25:25 +0100

    worked on contents of belleville and professional education

--- assets/js/belleville.js
+++ assets/js/belleville.js
@@ -96,3 +96,67 @@ let visitorCount = localStorage.getItem('visitorCount') || 0
     visitorCount++
     localStorage.getItem('visitorCount', visitorCount)
     document.getElementById('visitorCount').innerHTML = visitorCount;
+
+
+    const courses = [
+        {
+            name: "Dr. Olivia Patel",
+            value: "Director, Belleville Dentals",
+            photo: "https://img.freepik.com/free-photo/doctor-standing-with-folder-stethoscope_1291-16.jpg?size=626&ext=jpg&ga=GA1.1.1664086905.1681670100&semt=sph",
+        },
+        {
+            name: "Dr. Emily Carter",
+            value: "Head, Pediatric Dentistry",
+            photo: "https://img.freepik.com/free-photo/isolated-shotof-happy-successful-mature-senior-physician-wearing-medical-unifrom-stethoscope-having-cheerful-facial-expression-smiling-broadly-keeping-arms-crossed-chest_343059-2254.jpg?size=626&ext=jpg&ga=GA1.2.1664086905.1681670100&semt=ais",
+        },
+        {
+            name: "Dr. Mabel Restania",
+            value: "Director, Belleville School of Dentistry",
+            photo: "http://127.0.0.1:5501/assets/images/faculty_head.jpg",
+        },
+        {
+            name: "Dr. Jennifer Adams",
+            value: "Head, Cosmetic Dentistry",
+            photo: "https://img.freepik.com/free-photo/front-view-smiley-doctor-clinic_23-2149726935.jpg?t=st=1688698418~exp=1688699018~hmac=9e2ed0a3f685fef82d48e7c107d30394b8a3a66046b50378505845ded44fa285",
+        },
+        {
+            name: "Dr. Ethan Ramirez",
+            value: "Theatre Manager",
+            photo: "https://img.freepik.com/free-photo/cheerful-doctor-making-notes-looking-away_23-2147896151.jpg?size=626&ext=jpg&ga=GA1.1.1664086905.1681670100&semt=sph",
+        },
+        {
+            name: "Dr. Andrew Johnson",
+            value: "Chief Technologists",
+            photo: "https://img.freepik.com/free-photo/medium-shot-veterinarian-with-microscope_23-2149143897.jpg?w=1060&t=st=1688699926~exp=1688700526~hmac=7af89e1af64e14826c408c985fc5b1769f444d06a0cd0671d06f013e0182af84",
+        }
+    ];
+    
+    const modal_container = document.querySelector(".modal_container");
+    
+    for (let course of courses) {
+        const productBox = document.createElement("div");
+        productBox.classList.add("main");
+    
+        const productEl = document.createElement("div");
+        productEl.classList.add("box");
+    
+        const photoEl = document.createElement("img");
+        photoEl.src = course.photo;
+        photoEl.alt = course.name;
+        photoEl.classList.add("img");
+    
+        const nameEl = document.createElement("p");
+        nameEl.innerText = course.name;
+        nameEl.classList.add("name", "fw-bold");
+    
+        const valueEl = document.createElement("p");
+        valueEl.innerHTML = course.value;
+    
+        // const purchase = document.createElement("button");
+        // purchase.innerHTML = `Details`
+    
+        productEl.append(nameEl, valueEl);
+        productBox.append(photoEl, productEl)
+    
+        modal_container.appendChild(productBox);
+    }
